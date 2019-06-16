3eee503b00a149564b75ca3a850ec99b740b7d50
Ian Gacek <gacek004@umn.edu>
2019-06-16 18:15:05 -0500

    Everything sucks

--- keys.js
+++ keys.js
@@ -4,13 +4,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-exports.omdb = {
-    id: process.env.OMDB_ID,
-    secret: process.env.OMDB_SECRET
-}
-
-exports.bandsInTown = {
-    id: process.env.BIT_ID,
-    secret: process.env.BIT_ID
-}
