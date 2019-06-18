de53d155a186ea60fbf38d7a17bb59e030f842b7
Ian Gacek <gacek004@umn.edu>
2019-06-17 21:08:16 -0500

    Semi-functional Spotify search

--- liri.js
+++ liri.js
@@ -11,55 +11,82 @@ var appCommand = inputString[2];
 // Choses input
 var userInput = inputString[3];
 
-if (appCommand === "concert-this") {
-    console.log();
-} else if (appCommand === "spotify-this-song") {
-    console.log();
-} else if (appCommand === "movie-this") {
-    console.log();
-} else if (appCommand === "do-what-it-says") {
-    console.log();
-}
+function mySwitch(appCommand) {
+    switch (appCommand) {
+        // Calls the Spotify function
+        case "spotify-this-song":
+            getSpotify();
+            break;
+        // Calls the OMDB function    
+        case "movie-this":
+            getMovie();
+            break;
+        // Calls Other Function
+        case "do-what-it-says":
+            doWhat();
+            break;
+    }
 
-var getSpotify = function (songName) {
-    spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function (err, data) {
-        if (err) {
-            console.log('Error occurred: ' + err);
-            return;
-        }
-        console.log(data.tracks);
-    });
-}
-getSpotify();
+    // Spotify Search function
+    function getSpotify() {
+        spotify.search(
+            {
+                type: "track",
+                query: userInput
+            },
+            function (err, data) {
+                if (err) {
+                    console.log("Error occurred: " + err);
+                    return;
+                }
 
-var getMovie = function (movieName) {
-    var movieName = userInput;
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
-    axios.get(queryUrl).then(
-        function (response) {
-            console.log("Movie title: " + response.data.Title);
-            console.log("Year released: " + response.data.Year);
-            console.log("IMDB Rating: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
-            console.log("Countries movie was produced in: " + response.data.Country);
-            console.log("Language: " + response.data.Language);
-            console.log("Movie Plot: " + response.data.Plot);
-            console.log("Actors/Actresses in the movie " + response.data.Title + ": " + response.data.Actors);
-        })
-        .catch(function (error) {
-            if (error.response) {
-                console.log("---------------Data---------------");
-                console.log(error.response.data);
-                console.log("---------------Status---------------");
-                console.log(error.response.status);
-                console.log("---------------Status---------------");
-                console.log(error.response.headers);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log("Error", error.message);
+                var songs = data.tracks.items;
+
+                for (var i = 0; i < songs.length; i++) {
+                    console.log(i);
+                    console.log(data);
+                    console.log("song name: " + songs[i].name);
+                    console.log("preview song: " + songs[i].preview_url);
+                    console.log("album: " + songs[i].album.name);
+                    console.log("-----------------------------------");
+                }
             }
-            console.log(error.config);
-        });
+        );
+    };
+
+
+    // OMDB Search function
+    function getMovie() {
+        var movieName = userInput;
+        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+        axios.get(queryUrl).then(
+            function (response) {
+                console.log("----------");
+                console.log("Movie title: " + response.data.Title);
+                console.log("Year released: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.imdbRating);
+                console.log("Rotten Tomatoes Rating: " + response.data.tomatoRating);
+                console.log("Countries movie was produced in: " + response.data.Country);
+                console.log("Language: " + response.data.Language);
+                console.log("Movie Plot: " + response.data.Plot);
+                console.log("Actors/Actresses in the movie " + response.data.Title + ": " + response.data.Actors);
+                console.log("----------")
+            })
+            .catch(function (error) {
+                if (error.response) {
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    console.log(error.request);
+                } else {
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+    }
 }
-getMovie();
+mySwitch(appCommand);
